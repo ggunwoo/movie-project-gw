0249de89f8a5064a08015bad1bd6b40fa0f153bd
pgw6541 <pgw6541@gmail.com>
2024-03-20 16:46:29 +0900

    #3.4 Parallel Requests

---
+++ app/about-us/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "About Us"
+}
+
+export default function AboutUsLayout({ children,}: {children: React.ReactNode}) {
+  return (
+    <div>
+      {children}
+      &copy; Next JS is great!
+    </div>
+  )
+}
