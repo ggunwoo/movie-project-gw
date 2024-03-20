31226de350c6e2a1fdb35d3e16988a1510a7d397
pgw6541 <pgw6541@gmail.com>
2024-03-20 16:46:29 +0900

    #3.4 Parallel Requests

---
+++ app/(home)/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+export const metadata = {
+  title : "HOME",
+};
+
+export const API_URL = '***REMOVED***'
+
+async function getMovies(){
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  )
+}
