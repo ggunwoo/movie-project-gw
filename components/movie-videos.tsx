4fd0bc52fb500c93342f8464b334d76bfc2e9afd
pgw6541 <pgw6541@gmail.com>
2024-03-22 01:55:16 +0900

    Update movie-videos

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,11 +1,9 @@
-
 import { API_URL } from "../app/contants";
 import styles from "../styles/movie-videos.module.css"
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  // throw new Error("something broke...")
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
