38b7fdb0feb1e1c73ed188af1bd3b9e016262cd9
pgw6541 <pgw6541@gmail.com>
2024-03-22 02:24:21 +0900

    Update movie-videos

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,5 +1,5 @@
 import { API_URL } from "../app/contants";
-import styles from "../styles/movie-videos.module.css"
+import styles from "../styles/movie-videos.module.css";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
@@ -13,7 +13,13 @@ export default async function MovieVideos({ id }: { id: string }) {
   return (
     <div className={styles.container}>
       {videos.map((video) => (
-        <iframe key={video.id} src={`https:/youtube.com/embed/${video.key}`} title={video.name} allowFullScreen />
+        <iframe
+          key={video.id}
+          src={`https:/youtube.com/embed/${video.key}`}
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
       ))}
     </div>
   );
