846da984440c7f1e5a8808704778b738757c111b
pgw6541 <pgw6541@gmail.com>
2024-03-29 16:00:00 +0900

    interface companies Type

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,12 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+
+// production_companies
+interface companie {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
