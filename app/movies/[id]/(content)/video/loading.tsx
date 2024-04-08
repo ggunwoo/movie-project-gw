0c4ea6d9caa27add8983ee0e1f8d8d4e32a2909d
pgw6541 <pgw6541@gmail.com>
2024-04-08 23:11:12 +0900

    Use | skeleton - videos page

--- app/movies/[id]/(content)/video/loading.tsx
+++ app/movies/[id]/(content)/video/loading.tsx
@@ -1,5 +1,18 @@
-import Loader from "@/components/loader"
+import styles from "@/styles/loading/loading-content.module.scss"
 
 export default function Loading(){
-  return <Loader />
+  const videoLength = new Array(20).fill(0)
+  return (
+    <section className={styles.skeleton_videos}>
+      <h2>VIDEOS</h2>
+      <ul>
+        {videoLength.map((el, index) => (
+          <li key={index}>
+            <div className={styles.skeleton_iframe}></div>
+            <p className={styles.skeleton_title}></p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
