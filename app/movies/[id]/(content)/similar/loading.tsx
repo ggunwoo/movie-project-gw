6cbadff56ee7b4799b4836abe4163c88528af83a
pgw6541 <pgw6541@gmail.com>
2024-04-09 00:02:06 +0900

    Use | skeleton - similar page

--- app/movies/[id]/(content)/similar/loading.tsx
+++ app/movies/[id]/(content)/similar/loading.tsx
@@ -1,5 +1,22 @@
-import Loader from "@/components/loader"
+import styles from "@/styles/loading/loading-content.module.scss"
 
-export default function Loading(){
-  return <Loader />
+export default function Loading() {
+  const similarLength = new Array(20).fill(0);
+
+  return (
+    <section className={styles.skeleton_similar}>
+      <h2>YOU MIGHT ALSO LIKE</h2>
+      <ul>
+        {similarLength.map((el, index) => (
+          <li key={index}>
+            <figure className={styles.skeleton_poster}></figure>
+            <article className={styles.skeleton_title}>
+              <p></p>
+              <p></p>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
