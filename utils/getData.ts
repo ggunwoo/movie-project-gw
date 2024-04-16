10fda0216881c0d4bfb679dea411969fab33591a
pgw6541 <pgw6541@gmail.com>
2024-04-16 21:41:29 +0900

    Add | 영화api url - 환경변수(env) 설정

--- utils/getData.ts
+++ utils/getData.ts
@@ -1,25 +1,23 @@
-import { API_URL } from "./contents";
-
 // 영화 정보
 export async function getMovie(id: string) {
-  const response = await fetch(`${API_URL}/${id}`)
+  const response = await fetch(`${process.env.MOVIES_API_URL}/${id}`)
   return response.json(); 
 }
 
 // 크레딧
 export async function getCredits(id: string) {
-  const response = await fetch(`${API_URL}/${id}/credits`);
+  const response = await fetch(`${process.env.MOVIES_API_URL}/${id}/credits`);
   return response.json();
 }
 
 // 비디오
 export async function getVideos(id: string) {
-  const response = await fetch(`${API_URL}/${id}/videos`);
+  const response = await fetch(`${process.env.MOVIES_API_URL}/${id}/videos`);
   return response.json();
 }
 
 // 추쳔영화
 export async function getSimilar(id: string){
-  const response = await fetch(`${API_URL}/${id}/similar`)
+  const response = await fetch(`${process.env.MOVIES_API_URL}/${id}/similar`)
   return response.json()
 }
